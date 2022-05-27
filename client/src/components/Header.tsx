78b4c24d9cff1779538ff272fe1c8a4ad87497ee
hironomiu <hironomiu@gmail.com>
2022-05-27 13:18:33 +0900

    commited

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
   })
   const isSignIn = useReactiveVar(isSignInVar)
   return (
-    <header className="flex h-10 justify-between">
+    <header className="flex h-10 justify-between border-b-[1px] border-gray-300">
       <div className="ml-4 text-2xl">Header</div>
       <nav className="flex mr-4">
         <div className="text-2xl mr-2">Dummy</div>
