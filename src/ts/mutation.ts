491ae09125a15fa78e11f6737ec6ea84f626b7f9
hironomiu <hironomiu@gmail.com>
2022-06-27 00:29:08 +0900

    commited

--- src/ts/mutation.ts
+++ src/ts/mutation.ts
@@ -1,36 +1,7 @@
-import { Response, Request } from 'express'
-import { PrismaClient } from '@prisma/client'
 import { authMutation } from './mutations/authMutation'
-
-const prisma = new PrismaClient()
+import { booksMutation } from './mutations/booksMutation'
 
 export const mutation = {
   ...authMutation,
-  // Memo: Booksの更新(update)
-  updateBook: async (
-    parent: any,
-    args: { id: number; title: string; author: string },
-    context: {
-      req: Request & {
-        session: { userId: number; email: string; nickname: string }
-      }
-      res: Response
-      user: any
-    }
-  ) => {
-    console.log('create book called:', args)
-    // MEMO: SignInチェック
-    if (!context.user) return null
-    const book = await prisma.books.update({
-      where: {
-        id: Number(args.id),
-      },
-      data: {
-        title: args.title,
-        author: args.author,
-      },
-    })
-    console.log(book)
-    return { isSuccess: true, message: 'created', book: book }
-  },
+  ...booksMutation,
 }
---
+++ src/ts/mutations/booksMutation.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export const booksMutation = {
+  // Memo: Booksの更新(update)
+  updateBook: async (
+    parent: any,
+    args: { id: number; title: string; author: string },
+    context: {
+      req: Request & {
+        session: { userId: number; email: string; nickname: string }
+      }
+      res: Response
+      user: any
+    }
+  ) => {
+    console.log('create book called:', args)
+    // MEMO: SignInチェック
+    if (!context.user) return null
+    const book = await prisma.books.update({
+      where: {
+        id: Number(args.id),
+      },
+      data: {
+        title: args.title,
+        author: args.author,
+      },
+    })
+    console.log(book)
+    return { isSuccess: true, message: 'created', book: book }
+  },
+}
